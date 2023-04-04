14e267f0dd5d15ac0ea21fb60763918ee563baa8
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-04-04 14:19:19 +0300

    Add trace for opentelemetry

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
 use std::str::FromStr;
 
 use axum::body::BoxBody;
-use axum::http::header;
+use axum::extract::State;
+use axum::http::{header, HeaderMap};
 use axum::response::{IntoResponse, Response};
 use axum::routing::get;
 use axum::Router;
+use opentelemetry::trace::{get_active_span, Status};
+use opentelemetry::{
+    global,
+    trace::{FutureExt, Span, TraceContextExt, Tracer},
+    Context, KeyValue,
+};
 use reqwest::StatusCode;
 use serde::Deserialize;
 use tracing::{info, Level};
@@ -13,6 +20,11 @@ use tracing_subscriber::{filter::Targets, layer::SubscriberExt, util::Subscriber
 #[derive(Deserialize)]
 struct CatImage {
     url: String,
+}
+
+#[derive(Clone)]
+struct ServerState {
+    client: reqwest::Client,
 }
 
 #[tokio::main]
@@ -26,7 +38,11 @@ async fn main() {
         .with(filter)
         .init();
 
-    let app = Router::new().route("/", get(root_get));
+    let state = ServerState {
+        client: Default::default(),
+    };
+
+    let app = Router::new().route("/", get(root_get)).with_state(state);
     let addr = "0.0.0.0:8080".parse().unwrap();
     info!("Listening on {addr}");
     axum::Server::bind(&addr)
@@ -45,8 +61,31 @@ async fn main() {
     //println!("{art}");
 }
 
-async fn root_get() -> Response<BoxBody> {
-    match get_cat_ascii_art().await {
+async fn root_get(headers: HeaderMap, State(state): State<ServerState>) -> Response<BoxBody> {
+    let tracer = global::tracer("");
+    let mut span = tracer.start("root_get");
+    span.set_attribute(KeyValue::new(
+        "user_agent",
+        headers
+            .get(header::USER_AGENT)
+            .map(|h| h.to_str().unwrap_or_default().to_owned())
+            .unwrap_or_default(),
+    ));
+
+    root_get_inner(state)
+        .with_context(Context::current_with_span(span))
+        .await
+}
+
+async fn root_get_inner(state: ServerState) -> Response<BoxBody> {
+    let tracer = global::tracer("");
+
+    match get_cat_ascii_art(&state.client)
+        .with_context(Context::current_with_span(
+            tracer.start("get_cat_ascii_art"),
+        ))
+        .await
+    {
         Ok(art) => (
             StatusCode::OK,
             [(header::CONTENT_TYPE, "text/html; charset=utf-8")],
@@ -54,15 +93,46 @@ async fn root_get() -> Response<BoxBody> {
         )
             .into_response(),
         Err(e) => {
-            println!("Something went wrong: {e}");
+            get_active_span(|span| {
+                span.set_status(Status::Error {
+                    description: format!("{e}").into(),
+                })
+            });
             (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong").into_response()
         }
     }
 }
 
-async fn get_cat_ascii_art() -> color_eyre::Result<String> {
+async fn get_cat_ascii_art(client: &reqwest::Client) -> color_eyre::Result<String> {
+    let tracer = global::tracer("");
+
+    let image_bytes = download_file(client, "https://cataas.com/cat")
+        .with_context(Context::current_with_span(tracer.start("download_file")))
+        .await?;
+
+    let image = tracer.in_span("image::load_from_memory", |cx| {
+        let img = image::load_from_memory(&image_bytes)?;
+        cx.span()
+            .set_attribute(KeyValue::new("width", img.width() as i64));
+        cx.span()
+            .set_attribute(KeyValue::new("height", img.height() as i64));
+        Ok::<_, color_eyre::eyre::Report>(img)
+    })?;
+
+    let ascii_art = tracer.in_span("artem::convert", |_cx| {
+        artem::convert(
+            image,
+            artem::options::OptionBuilder::new()
+                .target(artem::options::TargetType::HtmlFile(true, true))
+                .build(),
+        )
+    });
+
+    Ok(ascii_art)
+}
+
+async fn get_cat_image_url(client: &reqwest::Client) -> color_eyre::Result<String> {
     let api_url = "https://api.thecatapi.com/v1/images/search";
-    let client = reqwest::Client::default();
 
     let image = client
         .get(api_url)
@@ -73,63 +143,16 @@ async fn get_cat_ascii_art() -> color_eyre::Result<String> {
         .await?
         .pop()
         .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+    Ok(image.url)
+}
 
-    let image_bytes = client
-        .get(image.url)
+async fn download_file(client: &reqwest::Client, url: &str) -> color_eyre::Result<Vec<u8>> {
+    let bytes = client
+        .get(url)
         .send()
         .await?
         .error_for_status()?
         .bytes()
         .await?;
-
-    let image = image::load_from_memory(&image_bytes)?;
-    let ascii_art = artem::convert(
-        image,
-        artem::options::OptionBuilder::new()
-            .target(artem::options::TargetType::HtmlFile(true, true))
-            .build(),
-    );
-
-    Ok(ascii_art)
-}
-
-async fn get_cat_image_url() -> color_eyre::Result<String> {
-    let api_url = "https://api.thecatapi.com/v1/images/search";
-    let res = reqwest::get(api_url).await?;
-    if !res.status().is_success() {
-        return Err(color_eyre::eyre::eyre!(
-            "The Cat API returned HTTP {}",
-            res.status()
-        ));
-    }
-    let mut images: Vec<CatImage> = res.json().await?;
-    let Some(image) = images.pop() else {
-        return Err(color_eyre::eyre::eyre!("The Cat API returned no images"));
-    };
-
-    Ok(image.url)
-}
-
-async fn get_cat_image_bytes() -> color_eyre::Result<Vec<u8>> {
-    let api_url = "https://api.thecatapi.com/v1/images/search";
-    let client = reqwest::Client::default();
-
-    let image = client
-        .get(api_url)
-        .send()
-        .await?
-        .error_for_status()?
-        .json::<Vec<CatImage>>()
-        .await?
-        .pop()
-        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
-
-    Ok(client
-        .get(image.url)
-        .send()
-        .await?
-        .error_for_status()?
-        .bytes()
-        .await?
-        .to_vec())
+    Ok(bytes.to_vec())
 }
