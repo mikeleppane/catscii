aaeffebec22905b9c3c673a54ccccb8f1d917b9d
Mikko Leppänen <mikko.leppanen@vaisala.com>
2023-04-04 13:02:13 +0300

    Showing cats in art form

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,44 @@ struct CatImage {
 
 #[tokio::main]
 async fn main() {
-    let url = get_cat_image_url().await.unwrap();
-    println!("The image is at {}", url);
+    // let url = get_cat_image_url().await.unwrap();
+    // println!("The image is at {}", url);
+    //
+    // let image_bytes = get_cat_image_bytes().await.unwrap();
+    // // only dump the first 200 bytes so our terminal survives the
+    // // onslaught. this will panic if the image has fewer than 200 bytes.
+    // println!("{:?}", &image_bytes[..200].hex_dump());
 
-    let image_bytes = get_cat_image_bytes().await.unwrap();
-    // only dump the first 200 bytes so our terminal survives the
-    // onslaught. this will panic if the image has fewer than 200 bytes.
-    println!("{:?}", &image_bytes[..200].hex_dump());
+    let art = get_cat_ascii_art().await.unwrap();
+    println!("{art}");
+}
+
+async fn get_cat_ascii_art() -> color_eyre::Result<String> {
+    let api_url = "https://api.thecatapi.com/v1/images/search";
+    let client = reqwest::Client::default();
+
+    let image = client
+        .get(api_url)
+        .send()
+        .await?
+        .error_for_status()?
+        .json::<Vec<CatImage>>()
+        .await?
+        .pop()
+        .ok_or_else(|| color_eyre::eyre::eyre!("The Cat API returned no images"))?;
+
+    let image_bytes = client
+        .get(image.url)
+        .send()
+        .await?
+        .error_for_status()?
+        .bytes()
+        .await?;
+
+    let image = image::load_from_memory(&image_bytes)?;
+    let ascii_art = artem::convert(image, artem::options::OptionBuilder::new().build());
+
+    Ok(ascii_art)
 }
 
 async fn get_cat_image_url() -> color_eyre::Result<String> {
